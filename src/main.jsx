3fb5301ed3c2c8c59dd06e3f1c3529f20adcc325
WorkerHarder171 <dadybima171@gmail.com>
2024-06-01 01:13:16 +0700

    fix update 01/06/2024

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+// import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
