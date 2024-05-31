85b9f64ce0d6815e1bb6740b7fa71571587e9cb4
WorkerHarder171 <dadybima171@gmail.com>
2024-06-01 01:16:16 +0700

    fix update 01/06/2024

--- src/components/jumbotron/Jumbotron.jsx
+++ src/components/jumbotron/Jumbotron.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import pp from "../../assets/pp.jpg";
+import pp from "@/assets/PP.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faSquareFacebook,
