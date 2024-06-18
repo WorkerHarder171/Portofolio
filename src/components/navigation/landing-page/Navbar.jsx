ace26dd6b6dcdfa1f4cf15114f40c9b6e2c3a075
WorkerHarder171 <dadybima171@gmail.com>
2024-06-18 20:10:56 +0700

    update 18/06/2024

--- src/components/navigation/landing-page/Navbar.jsx
+++ src/components/navigation/landing-page/Navbar.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState, useRef } from "react";
+import hamburger_active from "@/assets/hamburger-active.svg";
+import hamburger_non_active from "@/assets/hamburger-non-active.svg";
+import { useEffect, useState, useRef } from "react";
+import { Link } from "react-scroll";
 import { useInView } from "framer-motion";
 
-export const Navbar = () => {
+export function Navbar() {
+  const [toggleNavbar, setToggleNavbar] = useState(false);
   const [color, setColor] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
-  // Fungsi untuk mengubah warna navbar saat scroll
   const changeColor = () => {
-    if (window.scrollY >= 90) {
+    if (window.scrollY >= 72) {
       setColor(true);
     } else {
       setColor(false);
@@ -23,53 +27,93 @@ export const Navbar = () => {
     };
   }, []);
 
+  const menuItems = [
+    { text: "Beranda", link: "#" },
+    { text: "Fitur", link: "card" },
+    { text: "Tentang", link: "framePahlawan" },
+    { text: "Eksplorasi", link: "frameDaurUlang" },
+    { text: "FaQ", link: "question" },
+  ];
+
   return (
     <>
       <nav
         className={
           "p-5 w-full fixed transition-all duration-500 ease-in-out z-50 border border-[#2e3c51] " +
-          (color ? "bg-white shadow text-black" : "bg-[#16202de6]  border-none text-white")
+          (color
+            ? "bg-white shadow text-black"
+            : "bg-[#16202de6] border-none text-white")
         }
         ref={ref}
         style={{
           transform: isInView ? "none" : "translateY(-50px)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1)",
+          transition: "all 0.6s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
         }}
       >
-        <div className="flex justify-around items-center ">
-          {/* Logo */}
-          <div className="nav-brand">
-            <a href="#" className={"text-2xl tracking-wider font-bold"}>
-              dybim__
-            </a>
+        <div className="container mx-auto px-4 sm:px-8 lg:px-[72px] flex justify-between items-center py-3 sm:py-4">
+          <div className="w-28 order-1 sm:order-2 lg:order-1">
+            <div className="nav-brand">
+              <a href="#" className={"text-2xl tracking-wider font-bold"}>
+                dybim__
+              </a>
+            </div>
           </div>
-
-          {/* Navigation */}
-          <ul className="w-2/6 flex justify-around items-center text-lg">
-            <li className="nav-item">
-              <a href="#" className={"nav-links duration-500 ease-in-out hover:font-semibold hover:mx-5 hover:text-lg " + (color ? "hover:font-bold" : "hover:text-[#B895FD]")}>
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className={"nav-links duration-500 ease-in-out hover:font-semibold hover:mx-5 hover:text-lg " + (color ? "hover:font-bold" : "hover:text-[#B895FD]")}>
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className={"nav-links duration-500 ease-in-out hover:font-semibold hover:mx-5 hover:text-lg " + (color ? "hover:font-bold" : "hover:text-[#B895FD]")}>
-                Portofolio
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className={"nav-links duration-500 ease-in-out hover:font-semibold hover:mx-5 hover:text-lg " + (color ? "hover:font-bold" : "hover:text-[#B895FD]")}>
-                Certifications
-              </a>
-            </li>
+          <div
+            className="cursor-pointer order-2 sm:order-1 lg:hidden"
+            onClick={() => setToggleNavbar(toggleNavbar ? false : true)}
+          >
+            <img
+              className="w-9"
+              src={toggleNavbar ? hamburger_active : hamburger_non_active}
+              alt="toggle"
+            />
+          </div>
+          <div className="hidden lg:block lg:order-2">
+            <ul className="flex gap-14">
+              {menuItems.map((item, index) => (
+                <li key={index}>
+                  <Link
+                    activeClass="active"
+                    className={
+                      "nav-links duration-500 ease-in-out hover:font-semibold hover:mx-5 hover:text-lg " +
+                      (color ? "hover:font-bold" : "hover:text-[#B895FD]")
+                    }
+                    to={item.link}
+                    spy={true}
+                    smooth={true}
+                    offset={-55}
+                    duration={700}
+                    onSetActive={() => setActiveSection(item.link)}
+                  >
+                    {item.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className={`${toggleNavbar ? "block" : "hidden"} lg:hidden`}>
+          <ul className="text-start bg-white p-5">
+            {menuItems.map((item, index) => (
+              <li key={index} className="flex">
+                <Link
+                  activeClass="active"
+                  className="text-black hover:font-bold cursor-pointer px-4 sm:px-8 py-2"
+                  to={item.link}
+                  spy={true}
+                  smooth={true}
+                  offset={-60}
+                  duration={700}
+                  onSetActive={() => setActiveSection(item.link)}
+                >
+                  {item.text}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
     </>
   );
-};
+}
